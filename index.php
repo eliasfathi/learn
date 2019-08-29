ce34b3568d07d2a0929da93323588d4309beb390
unknown <eliasfathi@gmail.com>
2019-08-29 00:23:19 -0700

    master

--- index.php
+++ index.php
@@ -1,11 +1,89 @@
+<!DOCTYPE html>
 <html>
+
 <head>
-<h2>Test Program</h2>
+    <title>سایت تست</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href='css/style.css' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
 </head>
+
 <body>
-<?php
-echo "test";
-var_dump("test");
-?>
+    <div class="jumbotron text-center" style="margin-bottom:0%">
+        <h1> برنامه تستی </h1>
+        <p>متن تستی رسپانسیو </p>
+    </div>
+    <nav class="navbar navbar-expand-md bg-success navbar-dark">
+        <a class="navbar-brand" href="#">خانه</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggle-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">منوی 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">منوی 1</a>
+                </li>
+            </ul>
+        </div>
+
+    </nav>
+    <div class="container" style="margin-top:30px">
+        <div class="row">
+            <div class="col-md-4">
+                <h2> تست</h2>
+                <h5>تستی</h5>
+                <div class="fakeimg">Fake Image</div>
+                <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+                <h3>Some Links</h3>
+                <p>Lorem ipsum dolor sit ame.</p>
+                <ul class="nav nav-pills flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Active</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                </ul>
+                <hr class="d-sm-none">
+            </div>
+
+            <div class="col-md-4 text-center">
+                <h2>تست 2</h2>
+                <p> متن تستی </p>
+                <p> متن تستی </p>
+                <p> متن تستی </p>
+                <p> متن تستی </p>
+                
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="form-group">
+                    <label for="usr">نام کاربری</label>
+                    <input type="text" class="form-control" id="usr">
+                </div>
+                <div class="form-group">
+                    <label for="pwd">رمز عبور</label>
+                    <input type="password" class="form-control" id="pwd">
+                </div>
+                <button type="button" class="btn btn-info">ورود</button>
+            </div>
+        </div>
+    </div>
+    <div class="jumbotron text-center" style="margin-bottom:0">
+        <p>Footer</p>
+    </div>
 </body>
+
 </html>
