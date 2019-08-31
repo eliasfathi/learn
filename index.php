fb36b24450cbb9bc35e28ee65f9b402c073845a7
unknown <eliasfathi@gmail.com>
2019-08-31 19:35:12 +0430

    master

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 </head>
 
 <body>
+
     <div class="container">
         <div id="myapp">
             <p> {{message}} </p>
@@ -28,7 +29,7 @@
             }
         })
     </script>
-
+<p> test </p> 
 </body>
 
 </html>
