2a2f68997d5b1ea68a8c17fbb52ebf8b6fd95e3e
unknown <eliasfathi@gmail.com>
2019-08-31 00:03:04 -0700

    master

--- index.php
+++ index.php
@@ -6,82 +6,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link href='css/style.css' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery-3.4.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 </head>
 
 <body>
-    <div class="container-fluid bg-info" style="margin-top:30px">
-        <div class="row">
-            <div class="col-md-4">
-                <h2> تست</h2>
-                <h5 class="bg-success text-danger">تستی</h5>
-                <div class="fakeimg">Fake Image</div>
-                <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-                <h3>Some Links</h3>
-                <p>Lorem ipsum dolor sit ame.</p>
-                <ul class="nav nav-pills flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Active</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                </ul>
-                <hr class="d-sm-none">
-            </div>
-
-            <div class="col-md-4 text-center">
-                <h2>تست 2</h2>
-                <p> متن تستی </p>
-                <p> متن تستی </p>
-                <p> متن تستی </p>
-                <p> متن تستی </p>
-
-            </div>
-            <div class="col-md-4 text-center">
-                <div class="form-group">
-                    <label for="usr">نام کاربری</label>
-                    <input type="text" class="form-control" id="usr">
-                </div>
-                <div class="form-group">
-                    <label for="pwd">رمز عبور</label>
-                    <input type="password" class="form-control" id="pwd">
-                </div>
-                <button type="button" class="btn btn-info">ورود</button>
-            </div>
-        </div>
-    </div>
     <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item"> sfffasefsff</li>
-                    <li class="list-group-item avtive"> fqeqeqweqweqwe</li>
-                    <a href="#" class="list-group-item list-group-item-action">First item</a>
-                </ul>
-            </div>
-
-
-            <div class="col-md-6">
-                <button class="btn btn-primary">
-                    <span class="spinner-border spinner-border-sm"></span>
-                    Loading..
-                </button>
-            </div>
+        <div id="myapp">
+            <p> {{message}} </p>
+            <input v-model='message'>
         </div>
     </div>
-    <div class="jumbotron text-center" style="margin-bottom:0">
-        <p>Footer</p>
-    </div>
+    <script>
+        var app6 = new Vue({
+            el: '#myapp',
+            data: {
+                message: 'Hello Vue!'
+            }
+        })
+    </script>
+
 </body>
 
 </html>
